830b58c66fb64046d86773746671e7f5cec053a2
David Jimenez <davidrumsjb@icloud.com>
2020-10-17 22:41:31 +0900

    Working 2-way fpn

--- efficientPS/models/fpn.py
+++ efficientPS/models/fpn.py
@@ -1,20 +1,138 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-from .utilities import DepthSeparableConv2d
+from .utilities import DepthSeparableConv2d, MobileInvertedBottleneck, conv_1x1_bn
 
 
 class TwoWayFeaturePyramid(nn.Module):
     def __init__(self, activation=nn.GELU):
         super(TwoWayFeaturePyramid, self).__init__()
 
-        self.activation = activation()
-        self.block1 = DepthSeparableConv2d(
+        # Main branch
+
+        self.block1 = MobileInvertedBottleneck(
             in_channels=3,
-            out_channels=24,
-            kernel_size=3,
+            out_channels=48,
             stride=2,
-            padding=1,
-            dilation=1,
-            bias=False,
-        )
+            expand_ratio=1,
+        ) # output 48, 512, 1024
+        self.block2 = MobileInvertedBottleneck(
+            in_channels=48,
+            out_channels=24,
+            stride=1,
+            expand_ratio=1,
+        ) # output 24, 512, 1024
+        self.block3 = MobileInvertedBottleneck(
+            in_channels=24,
+            out_channels=40,
+            stride=2,
+            expand_ratio=1,
+        ) # output 40, 256, 512
+        self.block4 = MobileInvertedBottleneck(
+            in_channels=40,
+            out_channels=64,
+            stride=2,
+            expand_ratio=1,
+        ) # output 64, 128, 256
+        self.block5 = MobileInvertedBottleneck(
+            in_channels=64,
+            out_channels=128,
+            stride=2,
+            expand_ratio=1,
+        ) # output 128, 64, 128
+        self.block6 = MobileInvertedBottleneck(
+            in_channels=128,
+            out_channels=176,
+            stride=1,
+            expand_ratio=1,
+        ) # output 176, 64, 128
+        self.block7 = MobileInvertedBottleneck(
+            in_channels=176,
+            out_channels=304,
+            stride=2,
+            expand_ratio=1,
+        ) # output 304, 32, 64
+        self.block8 = MobileInvertedBottleneck(
+            in_channels=304,
+            out_channels=512,
+            stride=1,
+            expand_ratio=1,
+        ) # output 512, 32, 64
+        self.block9 = MobileInvertedBottleneck(
+            in_channels=512,
+            out_channels=2048,
+            stride=1,
+            expand_ratio=1,
+        ) # output 2048, 32, 64
+
+        # Bottom-up branch
+        self.times4_reduction_bu = conv_1x1_bn(40, 256)
+        self.downsample4 = nn.AdaptiveAvgPool2d((128, 256))
+        self.times8_reduction_bu = conv_1x1_bn(64, 256)
+        self.downsample8 = nn.AdaptiveAvgPool2d((64, 128))
+        self.times16_reduction_bu = conv_1x1_bn(176, 256)
+        self.downsample16 = nn.AdaptiveAvgPool2d((32, 64))
+        self.times32_reduction_bu = conv_1x1_bn(2048, 256)
+
+        # Top-down branch
+        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.times4_reduction_td = conv_1x1_bn(40, 256)
+        self.times8_reduction_td = conv_1x1_bn(64, 256)
+        self.times16_reduction_td = conv_1x1_bn(176, 256)
+        self.times32_reduction_td = conv_1x1_bn(2048, 256)
+
+        # Ps Separable Convolutions
+        self.p32conv = DepthSeparableConv2d(256, 256)
+        self.p16conv = DepthSeparableConv2d(256, 256)
+        self.p8conv = DepthSeparableConv2d(256, 256)
+        self.p4conv = DepthSeparableConv2d(256, 256)
+
+    def forward(self, inp):
+        # Main and bottom-up
+        x = self.block1(inp)
+        x = self.block2(x)
+        x = self.block3(x)
+
+        x_bu_1 = self.times4_reduction_bu(x)
+        x_td_4_ = self.times4_reduction_td(x)
+
+        x = self.block4(x)
+
+        x_bu_2 = self.times8_reduction_bu(x) + self.downsample4(x_bu_1)
+        x_td_3_ = self.times8_reduction_td(x)
+
+        x = self.block5(x)
+        x = self.block6(x)
+
+        x_bu_3 = self.times16_reduction_bu(x) + self.downsample8(x_bu_2)
+        x_td_2_ = self.times16_reduction_td(x)
+
+        x = self.block7(x)
+        x = self.block8(x)
+        x = self.block9(x)
+
+        x_bu_4 = self.times32_reduction_bu(x) + self.downsample16(x_bu_3)
+        x_td_1 = self.times32_reduction_td(x)
+
+        # Top-down branch computation
+        x_td_2 = x_td_2_ + self.upsample(x_td_1)
+        x_td_3 = x_td_3_ + self.upsample(x_td_2)
+        x_td_4 = x_td_4_ + self.upsample(x_td_3)
+
+        # Final Ps
+        p32 = self.p32conv(x_td_1 + x_bu_4)
+        p16 = self.p16conv(x_td_2 + x_bu_3)
+        p8 = self.p8conv(x_td_3 + x_bu_2)
+        p4 = self.p4conv(x_td_4 + x_bu_1)
+
+        return p32, p16, p8, p4
+
+
+if __name__ == "__main__":
+    fpn = TwoWayFeaturePyramid()
+    p32, p16, p8, p4 = fpn(torch.rand(3, 3, 1024, 2048))
+    print("p32", p32.shape)
+    print("p16", p16.shape)
+    print("p8", p8.shape)
+    print("p4", p4.shape)
--- efficientPS/models/utilities.py
+++ efficientPS/models/utilities.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 
+def outputSize(in_size, kernel_size, stride, padding):
+    output = int((in_size - kernel_size + 2*padding) / stride) + 1
+    return output
 
 class DepthSeparableConv2d(nn.Module):
     def __init__(
@@ -9,7 +12,7 @@ class DepthSeparableConv2d(nn.Module):
         out_channels,
         kernel_size=3,
         stride=1,
-        padding=0,
+        padding=1,
         dilation=1,
         bias=False,
     ):
@@ -33,3 +36,49 @@ class DepthSeparableConv2d(nn.Module):
         x = self.conv(x)
         x = self.depth_conv(x)
         return x
+
+
+# def conv_3x3_bn(in_channels, out_channels, stride):
+#     return nn.Sequential(
+#         nn.Conv2d(in_channels, out_channels, 3, stride, 1, bias=False),
+#         nn.BatchNorm2d(out_channels),
+#         nn.ReLU6(inplace=True)
+#     )
+
+
+def conv_1x1_bn(in_channels, out_channels, activation=nn.LeakyReLU):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU6(inplace=True)
+    )
+
+
+class MobileInvertedBottleneck(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, expand_ratio, activation=nn.ReLU6):
+        super(MobileInvertedBottleneck, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+
+        hidden_dim = int(in_channels * expand_ratio)
+        self.residual_shortcut = (self.stride == 1 and in_channels == out_channels)
+
+        self.conv = nn.Sequential(
+            # pw
+            nn.Conv2d(in_channels, hidden_dim, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(hidden_dim),
+            activation(inplace=True),
+            # dw
+            nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+            nn.BatchNorm2d(hidden_dim),
+            activation(inplace=True),
+            # pw-linear
+            nn.Conv2d(hidden_dim, out_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(out_channels),
+        )
+
+    def forward(self, x):
+        if self.residual_shortcut:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)

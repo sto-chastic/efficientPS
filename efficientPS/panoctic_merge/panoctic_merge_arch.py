6442672a9834a67399d39b0a5f428256fc1999f0
David Jimenez <d.jimenez298@uniandes.edu.co>
2020-11-06 21:41:35 +0900

    more stuff

--- efficientPS/panoctic_merge/panoctic_merge_arch.py
+++ efficientPS/panoctic_merge/panoctic_merge_arch.py
@@ -14,22 +14,33 @@ masked_logits = create_simple_logit(n_things, logit_size, logit_size)
 bboxes = create_uniform_bbox_pred(n_things, **bbox_data)
 class_pred = create_class_pred(n_things)
 confidence = create_confidence_levels(n_things)
-
 # filter all the masked logits that are less than the threshold
-filtered_logits, filtered_conf = filter_on_confidence(confidence, masked_logits, confidence_thresh)
+filtered_logits, filtered_conf, og_indices = filter_on_confidence(confidence, masked_logits, confidence_thresh)
 # sort based on confidence levels
-sorted_logits, sorted_conf = sort_by_confidence(filtered_logits, filtered_conf)
+sorted_logits, sorted_conf, og_indices = sort_by_confidence(filtered_logits, filtered_conf, og_indices)
 # scale the masked logits by bounding box and pad to original image size
-repadded_tensors = scale_pad_logits_with_bbox(sorted_logits, og_size, bboxes)
+MLa = scale_pad_logits_with_bbox(sorted_logits, og_size, bboxes)
 
-##############################################################
-#### FILTER THE REPADDED TENSORS WITH OVERLAP THRESH USING YOUR IOU FUNCTION ####
+################################################################################################
+#### FILTER THE MLa WITH OVERLAP THRESH USING YOUR IOU FUNCTION to get a new MLa ####
+#### MAKE SURE TO TRACK THE OG_INDICES WHEN YOU DO THE OVERLAP THRESHOLD FUNCTION ####
+################################################################################################
 
 # semantic segmentation route
-semantic_logit = create_simple_logit(n_stuff+n_things, og_size[0], og_size[1])
-
-semantic_prediction = get_semantic_prediction(semantic_logit)
+semantic_logit = create_simple_logit(n_things+n_stuff, og_size[0], og_size[1])
+semantic_prediction = logit_prediction(semantic_logit)
 
 # assume the stuff is in the initial channels, and the things in the final ones
-semantic_logit_stuff = semantic_logit[:n_stuff]
+semantic_logit_things = semantic_logit[:n_things]
+semantic_logit_stuff = semantic_logit[-n_stuff:]
+
+MLb = zero_out_nonbbox(semantic_logit_things[og_indices], bboxes[:, og_indices])
+
+fusion = panoctic_fusion(MLa, MLb)
+
+intermediate_logits = merge_fusion_semantic_things(semantic_logit_stuff, fusion.data.numpy())
+
+intermediate_prediction = logit_prediction(intermediate_logits)
+
+print(intermediate_prediction.shape)
 
--- efficientPS/panoctic_merge/panoctic_ops.py
+++ efficientPS/panoctic_merge/panoctic_ops.py
@@ -1,35 +1,38 @@
 import numpy as np
-from torch import Tensor, zeros
+from torch import Tensor, zeros, stack, sigmoid
 from torch.nn.functional import interpolate
 from typing import List, Tuple
 
 
 def filter_on_confidence(
     confidence: np.ndarray, logits: np.ndarray, thresh: float
-) -> Tuple[np.ndarray, np.ndarray]:
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     # not vectorized - sorry not sorry
     filtered_logits = []
     corresponding_confidence = []
+    corresponding_indices = []
     confidence = confidence.ravel()
     for i, el in enumerate(logits):
         if confidence[i] >= thresh:
             filtered_logits.append(el)
             corresponding_confidence.append(confidence[i])
-    return np.array(filtered_logits), np.array(corresponding_confidence)
+            corresponding_indices.append(i)
+    return np.array(filtered_logits), np.array(corresponding_confidence), np.array(corresponding_indices)
 
 
 def sort_by_confidence(
-    logits: np.ndarray, confidence: np.ndarray
-) -> Tuple[np.ndarray, np.ndarray]:
+    logits: np.ndarray, confidence: np.ndarray, og_indices: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     sorted_conf_inds = confidence.argsort()
     sorted_conf = confidence[sorted_conf_inds[::-1]]
     sorted_logits = logits[sorted_conf_inds[::-1]]
-    return sorted_logits, sorted_conf
+    sorted_og_indices = og_indices[sorted_conf_inds[::-1]]
+    return sorted_logits, sorted_conf, sorted_og_indices
 
 
 def scale_pad_logits_with_bbox(
     logits: np.ndarray, og_size: tuple, bboxes: np.ndarray
-) -> List[Tensor]:
+) -> Tensor:
     # also not vectorized, sorry
     out_tensor = []
     for (i, array) in enumerate(logits):
@@ -53,19 +56,58 @@ def scale_pad_logits_with_bbox(
         if eff_y < 0:
             h = int(h + eff_y)
             eff_y = 0
+        
+        repadded_tensor = zeros(og_size[0], og_size[1])
 
         # do nothing with out of scope bboxes
         if h <= 0 and w <= 0:
-            out_tensor.append(repadded_tensor.data.numpy())
-
-        repadded_tensor = zeros(og_size[0], og_size[1])
+            out_tensor.append(repadded_tensor)
+            continue
+        
         repadded_tensor[eff_y : eff_y + h, eff_x : eff_x + w] = scaled_tensor[
             eff_y : eff_y + h, eff_x : eff_x + w
         ]
         out_tensor.append(repadded_tensor)
-    return out_tensor
+    return stack(out_tensor)
 
 
-def get_semantic_prediction(semantic_logit: np.ndarray) -> np.ndarray:
-    return np.argmax(semantic_logit, axis=0)
+def logit_prediction(logits: np.ndarray) -> np.ndarray:
+    return np.argmax(logits, axis=0)
 
+def zero_out_nonbbox(logits: np.ndarray, bboxes: np.ndarray) -> Tensor:
+    out_tensor = []
+    for (i, array) in enumerate(logits):
+        tensor = Tensor(array)
+        x, y, w, h = bboxes[:, i]
+        h, w = int(h), int(w)
+
+        eff_x = int(x - w // 2)
+        eff_y = int(y - h // 2)
+
+        # adjust for patially out of scope bboxes
+        if eff_x < 0:
+            w = int(w + eff_x)
+            eff_x = 0
+
+        if eff_y < 0:
+            h = int(h + eff_y)
+            eff_y = 0
+
+        eff_tensor = zeros(array.shape[0], array.shape[1])
+
+        # do nothing with out of scope bboxes
+        if h <= 0 and w <= 0:
+            out_tensor.append(eff_tensor)
+        
+        eff_tensor[eff_y : eff_y + h, eff_x : eff_x + w] = tensor[
+            eff_y : eff_y + h, eff_x : eff_x + w
+        ]
+        out_tensor.append(eff_tensor)
+    return stack(out_tensor)
+
+
+def panoctic_fusion(MLa: Tensor, MLb: Tensor) -> Tensor:
+    return (sigmoid(MLa) + sigmoid(MLb)) * (MLa + MLa)
+
+def merge_fusion_semantic_things(array1: np.ndarray, array2: np.ndarray):
+    return np.concatenate((array1, array2))
